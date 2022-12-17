2f792a919238edcae1dc3588702832c7f91473a7
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2022-12-17 13:24:32 +0300

    Proje dosyası ekle.

---
+++ Services/ContactAPI/Contact.API/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet("{id}")]
+        public ContactDTO Get(int id) {
+            return _contactService.GetContactById(id);
+        }
+    }
+}
